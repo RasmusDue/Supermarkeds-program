e009b073dc73c72f8ffb400cfca19ccdf9008785
RasmusDue <rasduek@gmail.com>
2021-01-08 15:39:25 +0100

    0.1.1

--- Brugerflade.py
+++ Brugerflade.py
@@ -1,15 +1,41 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 import tkinter.ttk as ttk
+from PIL import ImageTk, Image
 
-class Book_gui(ttk.Frame):
+class Application(ttk.Frame):
     def __init__(self, master=None):
         ttk.Frame.__init__(self, master)
 
-        self.data = Books_data(False)
-        self.kurv = self.data.create_new_transaction()
+        self.pack()
+        self.Login_Screen()
 
-        self.build_GUI()
+    def Login_Screen(self):
+        # img = ImageTk.PhotoImage(Image.open("Other/martins-logo.png"))
+        # canvas.create_image(20,20, anchor=NW, image=img)
+        # img = ImageTk.PhotoImage(Image.open("Other/martins-logo.png"))
+        # self.create_image(20,20, anchor=NW, image=self.img)
+        # self.image = self.img
 
-        self.opdater_transaktions_tabel()
-        self.opdater_tabel()
+        # load = Image.open("Other/martins-logo.png")
+        # render = ImageTk.PhotoImage(load)
+        # img = Label(self, image=render)
+        # img.image = render
+        # img.place(x=0, y=0)
+
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+root.geometry("800x600")
+app = Application(master=root)
+app.master.title('Martin´s Supermarked')
+app.mainloop()
